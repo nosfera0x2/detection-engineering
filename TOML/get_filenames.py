5fe3b6a3631778c47d6d919e15e309e97b29c3ee
spence-rat <82817752+nosfera0x2@users.noreply.github.com>
2023-08-14 07:25:04 -0500

    Untracking directories

--- TOML/get_filenames.py
+++
@@ -1,8 +0,0 @@
-import os
-import tomllib
-
-for root, dirs, files in os.walk('C:\\Users\\Spencer.Brown\\OneDrive - Sophos Ltd\\Documents\\spenProjects\\Python\\Detection Engineering\\detection_rules'):
-    for file in files:
-        if file.endswith(".toml"):
-            with(open(file,"rb")) as toml:
-                alert = tomllib.load(toml)
